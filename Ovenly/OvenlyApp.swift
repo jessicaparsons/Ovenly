b9e9a75e329ccc30a2b1930d85d89b3c23af5543
Jessica Parsons <jess@junipercreative.co>
2025-02-09 15:55:59 -0800

    Initial Commit

---
+++ Ovenly/OvenlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OvenlyApp.swift
+//  Ovenly
+//
+//  Created by Jessica Parsons on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct OvenlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OvenlyTests/OvenlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  OvenlyTests.swift
+//  OvenlyTests
+//
+//  Created by Jessica Parsons on 2/9/25.
+//
+
+import Testing
+@testable import Ovenly
+
+struct OvenlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
